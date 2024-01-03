b471f030f8909b16890d2124e967976f5868317a
Shin <shinthantkaung@proton.me>
2024-01-03 14:11:41 +0630

    finish desktop design

---
+++ app/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("click", (event) => {
+  let targetElement = event.target;
+  
+  if (event.target.classList.contains("dropdown__arrow")) {
+    targetElement = event.target.parentElement;
+  }
+
+  console.log(targetElement);
+
+  const targetDataset = targetElement.dataset;
+  const targetDropDownCheck = targetDataset.dropdown;
+  const firstDropDown = document.querySelector(".dropdown--first");
+  const secondDropDown = document.querySelector(".dropdown--second");
+
+  const toggleDropdown = (dropdown, toggleValue) => {
+    if (toggleValue === "off") {
+      dropdown.classList.remove("hidden");
+      targetDataset.toggle = "on";
+    } else {
+      dropdown.classList.add("hidden");
+      targetDataset.toggle = "off";
+    }
+  };
+
+  if (targetDropDownCheck === "1") {
+    toggleDropdown(firstDropDown, targetDataset.toggle);
+    console.log(targetDataset.toggle);
+  } else if (targetDropDownCheck === "2") {
+    toggleDropdown(secondDropDown, targetDataset.toggle);
+    console.log(targetDataset.toggle);
+  }
+});
